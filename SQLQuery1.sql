6722eed450fec02ed320e29bf336a57d5391f22c
Prajwal <prajwal.zurunge12@gmail.com>
2021-11-14 13:04:33 +0530

    adding creating table program

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,12 @@
 # UC 1
 create database payroll_service
 use payroll_service
+
+# UC 2
+create table employee_payroll
+(
+id int not null,
+name varchar(100) not null,
+salary float not null,
+startdate date not null
+)
